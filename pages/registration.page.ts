1a0dad2d54ab8b5068cf7b1d3bcb9c325c34a935
yprokhorenko <55945105+yprokhorenko@users.noreply.github.com>
2025-10-30 18:07:21 +0200

    POM file created for  registration

--- pages/registration.page.ts
+++ pages/registration.page.ts
@@ -2,56 +2,49 @@ import { Page, Locator, expect } from "@playwright/test";
 
 
 type RegistrationFormData = {
-    firstName?: string;
-    middleName?: string;
-    lastName?: string;
-    email?: string;
-    phone?: string | number;
-    password?: string;
-    confirmPassword?: string;
-    captcha?: string | number;
+  surname?: string;
+  name_middleName?: string;
+  mail?: string;
+  phone?: string;
+  password? :string;
   };
 
 export class Registration {
 
     readonly page: Page;
-    private nameField: Locator;
-    private middleNameField: Locator;
-    private lastNameField: Locator;
-    private emailField: Locator;
-    private phoneField: Locator;
-    private passwordField: Locator;
-    private confirmPasswordField: Locator;
-    private captchaField: Locator;
+
+    private surname: Locator;
+    private name_middleName: Locator;
+    private mail: Locator;
+    private phone: Locator;
+    private password: Locator;
     private registerButton: Locator;
-    private alreadyRegisteredButton: Locator;
+    private authModal: Locator;
+    private registrationTab: Locator;
     private fields: Record<keyof RegistrationFormData, Locator>;
 
 
 
   constructor(page: Page) {
     this.page = page;
-    this.nameField = page.locator('[name="data[firstname]"]');
-    this.middleNameField = page.locator('[name="data[middlename]"]');
-    this.lastNameField = page.locator('[name="data[lastname]"]');
-    this.emailField = page.locator('[name="data[email]"]');
-    this.phoneField = page.locator('[name="data[phone]"]');
-    this.passwordField = page.locator('[name="data[password]"]');
-    this.confirmPasswordField = page.locator('[name="data[password_confirm]"]');
-    this.captchaField = page.locator('[name="captcha"]');
-    this.registerButton = page.locator('input[type="submit"][value="Зареєструватися"]');
-    this.alreadyRegisteredButton = page.locator('a[href="/login/"]');
+  
+    this.surname = page.locator("#reg_tab input[name=surname]");
+    this.name_middleName = page.locator("#reg_tab input[name=name]");
+    this.mail = page.locator("#reg_tab input[name=mail]");
+    this.phone = page.locator("#reg_tab input[name=phone]");
+    this.password = page.locator("#reg_tab input[name=password]");
+    this.registerButton = page.locator('#reg_tab #go_reg');
+    this.authModal = page.locator("#head #top_login");
+    this.registrationTab = page.locator(".lrm-switch-to--register");
+
 
     // Map of fields for the universal fillForm method
     this.fields = {
-        firstName: this.nameField,
-        middleName: this.middleNameField,
-        lastName: this.lastNameField,
-        email: this.emailField,
-        phone: this.phoneField,
-        password: this.passwordField,
-        confirmPassword: this.confirmPasswordField,
-        captcha: this.captchaField,
+        surname: this.surname,
+        name_middleName: this.name_middleName,
+        mail: this.mail,
+        phone: this.phone,
+        password: this.password
       };
 }
 
@@ -65,16 +58,26 @@ async fillForm(data: RegistrationFormData) {
         await locator.fill(String(value)); // convert everything to a string
       }
     }
+    await this.registerButton.click();
+  }
+
+  async openRegistrationTab() {
+    await this.authModal.click();
+    await this.registrationTab.click();
   }
 
   async clickRegisterButton() {
     await this.registerButton.click();
   }
 
-  async clickAlreadyRegistered() {
-    await this.alreadyRegisteredButton.click();
+  async assertUserIsRegistered() {
+    await this.page.waitForURL(/\/account/, { timeout: 10000 });
+    await expect(this.page).toHaveURL(/\/account/);
   }
 
 
+  async assertEmailAlreadyRegistered() {
+"Користувач з таким же значенням поля Емейл вже зареєстрований."
+  }
 
 }
